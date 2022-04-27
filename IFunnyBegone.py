a37c471033624f47a935e56d25df3b1fa6146a5e
Sgordon4 <SeanGordonkh@gmail.com>
2022-04-26 23:42:41 -0500

    Adding a cmd argument to separate into a different folder, while the default behavior is now to overwrite the original image with the stripped one for ease of use

--- IFunnyBegone.py
+++ IFunnyBegone.py
@@ -1,18 +1,29 @@
 import sys
 import os
+import argparse
 from PIL import Image, ImageChops
 
 
+parser = argparse.ArgumentParser(description = "Argument parser")
+parser.add_argument("filepath", help = "location of the images to be de-watermarked")
+parser.add_argument("-s", "--separate", action='store_true', help = "separate the stripped images into a new folder")
 
-directoryToCheck = sys.argv[1]
+argument = parser.parse_args()
 
 
-# Make the output directory if not exists
-cwd = os.getcwd()
-strippedDir = os.path.join(cwd,'stripped')
-if not os.path.exists(strippedDir):
-    os.mkdir(strippedDir)
-	
+
+directoryToCheck = argument.filepath
+
+if argument.separate:
+    # Make the output directory if it doesn't already exist
+    outputDir = os.path.join(directoryToCheck,'stripped')
+
+    if not os.path.exists(outputDir):
+        os.mkdir(outputDir)
+else:
+    outputDir = directoryToCheck
+
+
 
 watermarkName = "watermark.jpg"
 watermark = Image.open(watermarkName)
@@ -22,23 +33,28 @@ watermark = watermark.crop((width-136, height-20, width, height))
 
 # Iterate through the names of contents of the folder
 for image_name in os.listdir(directoryToCheck):
-	try:
-		# Create the full input path and read the file
-		image_path = os.path.join(directoryToCheck, image_name)
-		image = Image.open(image_path)
-		width, height = image.size
-		image = image.crop((width-136, height-20, width, height))
-		
-		# Calculate how similar the two images are
-		diff = ImageChops.difference(watermark, image)
-		average = sum( diff.resize((1, 1)).getpixel((0, 0)) )
-		
-		
-		if(average < 40):
-			image = Image.open(image_path)
-			stripped_img = image.crop((0, 0, width, height-20))
-			new_image = os.path.join(strippedDir, image_name)
-			stripped_img.save(new_image)
-			
-	except:
-		print(sys.exc_info()[0])
+    try:
+        # Create the full input path and read the file
+        image_path = os.path.join(directoryToCheck, image_name)
+        image = Image.open(image_path)
+        width, height = image.size
+        image = image.crop((width-136, height-20, width, height))
+
+        # Calculate how similar the two images are
+        diff = ImageChops.difference(watermark, image)
+        average = sum( diff.resize((1, 1)).getpixel((0, 0)) )
+
+
+        if(average < 40):
+            image = Image.open(image_path)
+            stripped_img = image.crop((0, 0, width, height-20))
+
+            new_image = os.path.join(outputDir, image_name)
+
+            stripped_img.save(new_image)
+
+
+    # Errors are generally from running into non-images in the folder to strip,
+    #  so this just skips those items
+    except:
+        print(sys.exc_info()[0])
--- README.md
+++ README.md
@@ -1,7 +1,28 @@
 This elite tier program is built for the sole purpose of stealing memes.
 
-To use this gift from God himself, the exe can be run on command line with:<br />
-<pre><code> IFunnyBegone.exe "C:\...\DIRECTORY_WITH_PICTURES" </code></pre>
 
-Otherwise, the command to run the base python file is:<br />
-<pre><code> python IFunnyBegone.py "C:\...\DIRECTORY_WITH_PICTURES" </code></pre>
+Required arguments:
+```
+filepath:                 Directory in which to de-watermark images
+```
+
+Optional arguments:
+```
+-s, --separate:        Separate the stripped images into a new folder
+```
+<br/>
+
+To use this gift from God himself, the exe can be run on Windows command line with:<br />
+```
+IFunnyBegone.exe "C:\...\DIRECTORY_WITH_PICTURES"
+OR
+IFunnyBegone.exe "C:\...\DIRECTORY_WITH_PICTURES" -s
+```
+
+
+Otherwise, the command to run the base python file is:
+```
+python IFunnyBegone.py "C:\...\DIRECTORY_WITH_PICTURES"
+OR
+python IFunnyBegone.py "C:\...\DIRECTORY_WITH_PICTURES" -s
+```
